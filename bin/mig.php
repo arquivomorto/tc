4458fe855b1584a1c1bdde5a4658f7d691228dc4
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-06-29 09:46:50 -0300

    cfg/cfg.php <- tablesDir

--- bin/mig.php
+++ bin/mig.php
@@ -3,9 +3,9 @@ require './cfg.php';
 require 'Mig.php';
 
 $db=require $INC.'/db.php';
-$tableDirectory = $ROOT.'/tables';
+$tableDir= $ROOT.'/'.$cfg['tablesDir'];
 $dbType=$cfgDB[$cfg['defaultDB']]['type'];
-$mig = new Mig($db->pdo, $tableDirectory, $dbType);
+$mig = new Mig($db->pdo, $tableDir, $dbType);
 system("clear");
 print 'executando migrations...' . PHP_EOL;
 if ($mig->run()) {
